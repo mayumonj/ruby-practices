b2e49280bb5b23525bcb63285e8029ec06223814
mayumonj <>
2022-05-14 17:20:59 +0900

    年・月以外の引数が入力された時に、macのcalコマンドと同様のエラーメッセージを出す

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -4,13 +4,23 @@ require 'date'
 opt = OptionParser.new
 params = {}
 
-opt.on('-m VAL') {|v| params[:m] = v.to_i}
-opt.on('-y VAL') {|v| params[:y] = v.to_i}
+opt.on('-m VAL') {|v| params[:m] = v}
+opt.on('-y VAL') {|v| params[:y] = v}
 
 opt.parse!(ARGV)
 
-params[:y].nil? ? year = Date.today.year : year = params[:y]
-params[:m].nil? ? month = Date.today.month : month = params[:m]
+if params[:y] != nil and (params[:y].to_i < 1 or params[:y].to_i > 9999)
+  puts "year `#{params[:y]}' not in range 1..9999"
+  return
+end
+
+if params[:m] != nil and (params[:m].to_i < 1 or params[:m].to_i > 12)
+  puts "#{params[:m]} is neither a month number (1..12) nor a name"
+  return
+end
+
+params[:y].nil? ? year = Date.today.year : year = params[:y].to_i
+params[:m].nil? ? month = Date.today.month : month = params[:m].to_i
 
 start_date = Date.new(year,month,1)
 end_date = Date.new(year,month,-1)
