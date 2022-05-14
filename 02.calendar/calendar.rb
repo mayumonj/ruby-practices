14026cf22eaaa059d6487656e0f890763d309aaf
mayumonj <>
2022-05-14 16:33:51 +0900

    リファクタ（表示対象月の最終日の指定方法を、dayに-1を設定する形に修正。if-elseを三項演算子に修正。自分用のコメントを削除。）

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -13,31 +13,19 @@ params[:y].nil? ? year = Date.today.year : year = params[:y]
 params[:m].nil? ? month = Date.today.month : month = params[:m]
 
 start_date = Date.new(year,month,1)
-end_date = start_date.next_month.prev_day
+end_date = Date.new(year,month,-1)
 
-# 年月の行を出力する
 print "      #{month}月 #{year}\n"
-
-# 曜日の行を出力する
 print "日 月 火 水 木 金 土\n"
 
-# 1日目を出力する（2文字右寄せ）
 date = start_date
+
 print "#{"   "*date.wday}#{date.day.to_s.rjust(2)}"
-if date.saturday?
-  print "\n"
-else
-  print " "
-end
+print date.saturday? ?  "\n" : " "
 date = date.next_day
 
-# 2日目以降を出力する、end_dateまできたら終了
 while date <= end_date
   print "#{date.day.to_s.rjust(2)}"
-  if date.saturday?
-    print "\n"
-  else
-    print " "
-  end
+  print date.saturday? ?  "\n" : " "
   date = date.next_day
 end
