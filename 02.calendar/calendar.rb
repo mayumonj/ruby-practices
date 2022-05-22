21d119fc959853cbf19a588c000fb108121e2ba3
mayumonj <>
2022-05-22 12:53:45 +0900

    カレンダーのプログラム(ruby) - Rubyらしい書き方に改善

--- 02.calendar/calendar.rb
+++ 02.calendar/calendar.rb
@@ -4,38 +4,35 @@ require 'date'
 opt = OptionParser.new
 params = {}
 
-opt.on('-m VAL') {|v| params[:m] = v}
-opt.on('-y VAL') {|v| params[:y] = v}
+opt.on('-m VAL') { |v| params[:m] = v }
+opt.on('-y VAL') { |v| params[:y] = v }
 
 opt.parse!(ARGV)
 
-if params[:y] != nil and (params[:y].to_i < 1 or params[:y].to_i > 9999)
+if !params[:y].nil? && (params[:y].to_i < 1 || params[:y].to_i > 9999)
   puts "year `#{params[:y]}' not in range 1..9999"
   return
 end
 
-if params[:m] != nil and (params[:m].to_i < 1 or params[:m].to_i > 12)
+if !params[:m].nil? && (params[:m].to_i < 1 || params[:m].to_i > 12)
   puts "#{params[:m]} is neither a month number (1..12) nor a name"
   return
 end
 
-params[:y].nil? ? year = Date.today.year : year = params[:y].to_i
-params[:m].nil? ? month = Date.today.month : month = params[:m].to_i
+year = params[:y].nil? ? Date.today.year : params[:y].to_i
+month = params[:m].nil? ? Date.today.month : params[:m].to_i
 
-start_date = Date.new(year,month,1)
-end_date = Date.new(year,month,-1)
+start_date = Date.new(year, month, 1)
+end_date = Date.new(year, month, -1)
 
-print "      #{month}月 #{year}\n"
-print "日 月 火 水 木 金 土\n"
+puts "      #{month}月 #{year}"
+puts '日 月 火 水 木 金 土'
 
 date = start_date
 
-print "#{"   "*date.wday}#{date.day.to_s.rjust(2)}"
-print date.saturday? ?  "\n" : " "
-date = date.next_day
-
+print ('   ' * date.wday).to_s
 while date <= end_date
-  print "#{date.day.to_s.rjust(2)}"
-  print date.saturday? ?  "\n" : " "
+  print date.day.to_s.rjust(2).to_s
+  print date.saturday? ? "\n" : ' '
   date = date.next_day
 end
