484bbd7be8766f103535b44094b5ff106633c3c1
mayumonj <>
2022-05-22 17:58:22 +0900

    ロジックをリファクタした

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -1,56 +1,47 @@
-# frozen_string_literal: true
-
-# 引数をとる
-score = ARGV[0]
-
-# 1投ごとに分割する
-scores = score.split(',')
-
-# 数字に変換
-shots = []
-scores.each do |s|
-  if s == 'X' # strike
-    shots << 10
-    shots << 0
-  else
-    shots << s.to_i
-  end
+def strike?(frame)
+  frame[0] == 10
 end
 
-# フレームごとに分割
-frames = []
-shots.each_slice(2) do |s|
-  frames << s
+def spare?(frame)
+  !strike?(frame) && frame.sum == 10
 end
+
+score_str = ARGV[0]
+scores = score_str.split(',')
+shots = scores.flat_map do |s|
+  s == 'X' ? [10, 0] : s.to_i
+end
+
+frames = shots.each_slice(2).to_a
 
 if frames.length > 10
-  frame10 = []
-  frames.drop(9).each do |frame|
-    frame.delete(0) if frame == [10, 0]
-    frame10 += frame
+  final_frame = frames[9..].flat_map do |frame|
+    frame[0] == 10 ? 10 : frame
   end
-  frames = frames.take(9).append(frame10)
+  frames = [*frames[..8], final_frame]
 end
 
-# 得点計算
 point = 0
-(0..9).each do |i|
-  point +=  if i <= 8 && frames[i][0] == 10 # strike
-              # ストライクのフレームの得点は次の2投の点を加算する（最終フレームは加算なし）
-              if frames[i + 1][0] == 10
-                if i == 8
-                  10 + frames[i + 1][0] + frames[i + 1][1]
-                else
-                  10 + frames[i + 1][0] + frames[i + 2][0]
-                end
-              else
-                10 + frames[i + 1][0] + frames[i + 1][1]
-              end
-            elsif i <= 8 && frames[i].sum == 10 # spare
-              # スペアのフレームの得点は次の1投の点を加算する（最終フレームは加算なし）
-              10 + frames[i + 1][0]
-            else
-              frames[i].sum
-            end
+(0..8).each do |i|
+  current_frame = frames[i]
+  next_frame = frames[i + 1]
+  after_next_frame = frames[i + 2]
+  def open_frame?(frame) = !(strike?(frame) || spare?(frame))
+  def one_before_final_frame?(frame_index) = (frame_index == 8)
+
+  point += if open_frame?(current_frame)
+             current_frame.sum
+           elsif spare?(current_frame)
+             10 + next_frame[0]
+           elsif strike?(current_frame) && !strike?(next_frame)
+             10 + next_frame[0] + next_frame[1]
+           elsif strike?(current_frame) && strike?(next_frame)
+             if one_before_final_frame?(i)
+               10 + next_frame[0] + next_frame[1]
+             else
+               10 + next_frame[0] + after_next_frame[0]
+             end
+           end
 end
-puts point
+
+puts point + frames[9].sum
