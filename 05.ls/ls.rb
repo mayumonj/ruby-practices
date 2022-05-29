42769ed21ee4f6277c20297e90f9461651cec23d
mayumonj <>
2022-05-29 16:47:48 +0900

    リファクタリング

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 NUMBER_OF_COLUMNS = 3
 DEFAULT_PADDING = 25
 DEFAULT_BUFFER = 3
@@ -17,27 +19,23 @@ def main
     exit
   end
 
-  file_names = Dir.glob('*')
-  display_filenames(file_names) unless file_names.empty?
+  content_names = Dir.glob('*')
+  display_filenames(content_names) unless content_names.empty?
 end
 
-def display_filenames(file_names)
-  padding = get_padding(file_names)
-  number_of_rows = (file_names.length / NUMBER_OF_COLUMNS.to_f).ceil
+def display_filenames(content_names)
+  padding = get_padding(content_names)
+  number_of_rows = (content_names.length / NUMBER_OF_COLUMNS.to_f).ceil
   (0..number_of_rows - 1).each do |n|
-    (0..file_names.length - 1).each do |i|
-      print file_names[i].ljust(padding) if i % number_of_rows == n
+    (0..content_names.length - 1).each do |i|
+      print content_names[i].ljust(padding) if i % number_of_rows == n
     end
     print "\n"
   end
 end
 
-def get_padding(file_names)
-  padding = DEFAULT_PADDING
-  file_names.each do |file_name|
-    padding = [file_name.length, padding].max
-  end
-  padding + DEFAULT_BUFFER
+def get_padding(content_names)
+  [DEFAULT_PADDING, content_names.map(&:length).max].max + DEFAULT_BUFFER
 end
 
 main if __FILE__ == $PROGRAM_NAME
