a513af017c6139026eb8d46bec99152e9f649424
mayumonj <40317050+mayumonj@users.noreply.github.com>
2022-11-05 12:13:21 +0900

    ロジックの変更に合わせてテストを修正

--- 08.ls_object/test/ls_test.rb
+++ 08.ls_object/test/ls_test.rb
@@ -14,90 +14,91 @@ class LsTest < Minitest::Test
 
   def setup
     Dir.chdir(CURRENT_DIRECTORY)
-    @ls = Ls.new
-    @content_factory = ContentFactory.new
   end
 
   def test_target_current_directory
     Dir.chdir('./current_dir')
-    contents, _message = @content_factory.create_content(nil)
+    contents = ContentFactory.create_contents(nil)
     assert_equal [
-      'file1.txt  '
-    ], @ls.display({}, contents)
+      'file1.txt'
+    ], Ls.display({}, contents)
   end
 
   def test_target_file
-    _contents, message = @content_factory.create_content('./file_a.txt')
-    assert_equal './file_a.txt', message
+    contents = ContentFactory.create_contents('./file_a.txt')
+    assert_equal [
+      './file_a.txt'
+    ], Ls.display({}, contents)
   end
 
   def test_target_empty_directory
-    contents, _message = @content_factory.create_content('./empty_dir')
-    assert_nil @ls.display({}, contents)
+    contents = ContentFactory.create_contents('./empty_dir')
+    assert_nil Ls.display({}, contents)
   end
 
   def test_target_not_exist_directory
-    _contents, message = @content_factory.create_content('not-exist')
-    assert_equal 'ls: not-exist: No such file or directory', message
+    ContentFactory.create_contents('not-exist')
+  rescue ArgumentError => e
+    assert_equal 'ls: not-exist: No such file or directory', e.message
   end
 
   def test_show_alias_and_directory
-    contents, _message = @content_factory.create_content('./alias_and_dir')
-    assert_equal ['alias_a    child_dir  file1.txt  '], @ls.display({}, contents)
+    contents = ContentFactory.create_contents('./alias_and_dir')
+    assert_equal ['alias_a    child_dir  file1.txt'], Ls.display({}, contents)
   end
 
   def test_show_1_content
-    contents, _message = @content_factory.create_content('./content_1_dir')
+    contents = ContentFactory.create_contents('./content_1_dir')
     assert_equal [
-      'file1.txt  '
-    ], @ls.display({}, contents)
+      'file1.txt'
+    ], Ls.display({}, contents)
   end
 
   def test_show_2_contents
-    contents, _message = @content_factory.create_content('./contents_2_dir')
+    contents = ContentFactory.create_contents('./contents_2_dir')
     assert_equal [
-      'file1.txt  file2.txt  '
-    ], @ls.display({}, contents)
+      'file1.txt  file2.txt'
+    ], Ls.display({}, contents)
   end
 
   def test_show_3_contents
-    contents, _message = @content_factory.create_content('./contents_3_dir')
+    contents = ContentFactory.create_contents('./contents_3_dir')
     assert_equal [
-      'file1.txt  file2.txt  file3.txt  '
-    ], @ls.display({}, contents)
+      'file1.txt  file2.txt  file3.txt'
+    ], Ls.display({}, contents)
   end
 
   def test_show_4_contents
-    contents, _message = @content_factory.create_content('./contents_4_dir')
+    contents = ContentFactory.create_contents('./contents_4_dir')
     assert_equal [
-      'file1.txt  file3.txt  ',
-      'file2.txt  file4.txt  '
-    ], @ls.display({}, contents)
+      'file1.txt  file3.txt',
+      'file2.txt  file4.txt'
+    ], Ls.display({}, contents)
   end
 
   def test_show_5_contents
-    contents, _message = @content_factory.create_content('./contents_5_dir')
+    contents = ContentFactory.create_contents('./contents_5_dir')
     assert_equal [
-      'file1.txt  file3.txt  file5.txt  ',
-      'file2.txt  file4.txt  '
-    ], @ls.display({}, contents)
+      'file1.txt  file3.txt  file5.txt',
+      'file2.txt  file4.txt'
+    ], Ls.display({}, contents)
   end
 
   def test_show_6_contents
-    contents, _message = @content_factory.create_content('./contents_6_dir')
+    contents = ContentFactory.create_contents('./contents_6_dir')
     assert_equal [
-      'file1.txt  file3.txt  file5.txt  ',
-      'file2.txt  file4.txt  file6.txt  '
-    ], @ls.display({}, contents)
+      'file1.txt  file3.txt  file5.txt',
+      'file2.txt  file4.txt  file6.txt'
+    ], Ls.display({}, contents)
   end
 
   def test_show_long_name
-    contents, _message = @content_factory.create_content('./having_long_name_file')
+    contents = ContentFactory.create_contents('./having_long_name_file')
     assert_equal [
-      'file1.txt                            file4.txt                            looooooooooooooooonoooooog_name.txt  ',
-      'file2.txt                            file5.txt                            ',
-      'file3.txt                            file6.txt                            '
-    ], @ls.display({}, contents)
+      'file1.txt                            file4.txt                            looooooooooooooooonoooooog_name.txt',
+      'file2.txt                            file5.txt',
+      'file3.txt                            file6.txt'
+    ], Ls.display({}, contents)
   end
 end
 
@@ -106,27 +107,25 @@ class LsAoptionTest < Minitest::Test
 
   def setup
     Dir.chdir(CURRENT_DIRECTORY)
-    @ls = Ls.new
-    @content_factory = ContentFactory.new
   end
 
   def test_target_current_directory_option_a
     Dir.chdir('./current_dir')
-    contents, _message = @content_factory.create_content(nil)
-    assert_equal ['.          file1.txt  '], @ls.display({ a: true }, contents)
+    contents = ContentFactory.create_contents(nil)
+    assert_equal ['.          file1.txt'], Ls.display({ a: true }, contents)
   end
 
   def test_target_empty_directory_option_a
-    contents, _message = @content_factory.create_content('./empty_dir')
-    assert_equal ['.  '], @ls.display({ a: true }, contents)
+    contents = ContentFactory.create_contents('./empty_dir')
+    assert_equal ['.'], Ls.display({ a: true }, contents)
   end
 
   def test_show_alias_and_directory_option_a
-    contents, _message = @content_factory.create_content('./alias_and_dir')
+    contents = ContentFactory.create_contents('./alias_and_dir')
     assert_equal [
-      '.          alias_a    file1.txt  ',
-      '.DS_Store  child_dir  '
-    ], @ls.display({ a: true }, contents)
+      '.          alias_a    file1.txt',
+      '.DS_Store  child_dir'
+    ], Ls.display({ a: true }, contents)
   end
 end
 
@@ -135,20 +134,18 @@ class LsRoptionTest < Minitest::Test
 
   def setup
     Dir.chdir(CURRENT_DIRECTORY)
-    @ls = Ls.new
-    @content_factory = ContentFactory.new
   end
 
   def test_target_empty_directory_option_r
-    contents, _message = @content_factory.create_content('./empty_dir')
-    assert_nil @ls.display({ r: true }, contents)
+    contents = ContentFactory.create_contents('./empty_dir')
+    assert_nil Ls.display({ r: true }, contents)
   end
 
   def test_show_contents_option_r
-    contents, _message = @content_factory.create_content('./alias_and_dir')
+    contents = ContentFactory.create_contents('./alias_and_dir')
     assert_equal [
-      'file1.txt  child_dir  alias_a    '
-    ], @ls.display({ r: true }, contents)
+      'file1.txt  child_dir  alias_a'
+    ], Ls.display({ r: true }, contents)
   end
 end
 
@@ -157,50 +154,48 @@ class LsMultiOptionsTest < Minitest::Test
 
   def setup
     Dir.chdir(CURRENT_DIRECTORY)
-    @ls = Ls.new
-    @content_factory = ContentFactory.new
   end
 
   def test_show_contents_option_ar
-    contents, _message = @content_factory.create_content('./alias_and_dir')
+    contents = ContentFactory.create_contents('./alias_and_dir')
     assert_equal [
-      'file1.txt  alias_a    .          ',
-      'child_dir  .DS_Store  '
-    ], @ls.display({ r: true, a: true }, contents)
+      'file1.txt  alias_a    .',
+      'child_dir  .DS_Store'
+    ], Ls.display({ r: true, a: true }, contents)
   end
 
   def test_show_contents_option_al
-    contents, _message = @content_factory.create_content('./alias_and_dir')
+    contents = ContentFactory.create_contents('./alias_and_dir')
     assert_equal [
       'total 32',
-      'drwxr-xr-x  6  UserName  staff    192  09 08 21:42  .',
+      'drwxr-xr-x  6  UserName  staff    192  10 22 10:36  .',
       '-rw-r--r--  1  UserName  staff  10244  09 06 20:53  .DS_Store',
-      '-rw-r--r--  1  UserName  staff    936  09 08 21:42  alias_a',
-      'drwxr-xr-x  3  UserName  staff     96  09 08 21:42  child_dir',
-      '-rw-r--r--  1  UserName  staff      0  09 08 21:42  file1.txt'
-    ], @ls.display({ a: true, l: true }, contents)
+      '-rw-r--r--  1  UserName  staff    936  10 22 10:36  alias_a',
+      'drwxr-xr-x  3  UserName  staff     96  10 22 10:36  child_dir',
+      '-rw-r--r--  1  UserName  staff      0  10 22 10:36  file1.txt'
+    ], Ls.display({ a: true, l: true }, contents)
   end
 
   def test_show_contents_option_lr
-    contents, _message = @content_factory.create_content('./alias_and_dir')
+    contents = ContentFactory.create_contents('./alias_and_dir')
     assert_equal [
       'total 8',
-      '-rw-r--r--  1  UserName  staff    0  09 08 21:42  file1.txt',
-      'drwxr-xr-x  3  UserName  staff   96  09 08 21:42  child_dir',
-      '-rw-r--r--  1  UserName  staff  936  09 08 21:42  alias_a'
-    ], @ls.display({ l: true, r: true }, contents)
+      '-rw-r--r--  1  UserName  staff    0  10 22 10:36  file1.txt',
+      'drwxr-xr-x  3  UserName  staff   96  10 22 10:36  child_dir',
+      '-rw-r--r--  1  UserName  staff  936  10 22 10:36  alias_a'
+    ], Ls.display({ l: true, r: true }, contents)
   end
 
   def test_show_contents_option_arl
-    contents, _message = @content_factory.create_content('./alias_and_dir')
+    contents = ContentFactory.create_contents('./alias_and_dir')
     assert_equal [
       'total 32',
-      '-rw-r--r--  1  UserName  staff      0  09 08 21:42  file1.txt',
-      'drwxr-xr-x  3  UserName  staff     96  09 08 21:42  child_dir',
-      '-rw-r--r--  1  UserName  staff    936  09 08 21:42  alias_a',
+      '-rw-r--r--  1  UserName  staff      0  10 22 10:36  file1.txt',
+      'drwxr-xr-x  3  UserName  staff     96  10 22 10:36  child_dir',
+      '-rw-r--r--  1  UserName  staff    936  10 22 10:36  alias_a',
       '-rw-r--r--  1  UserName  staff  10244  09 06 20:53  .DS_Store',
-      'drwxr-xr-x  6  UserName  staff    192  09 08 21:42  .'
-    ], @ls.display({ a: true, r: true, l: true }, contents)
+      'drwxr-xr-x  6  UserName  staff    192  10 22 10:36  .'
+    ], Ls.display({ a: true, r: true, l: true }, contents)
   end
 end
 
@@ -226,22 +221,20 @@ class LsLOptionTest < Minitest::Test
 
   def setup
     Dir.chdir(CURRENT_DIRECTORY)
-    @ls = Ls.new
-    @content_factory = ContentFactory.new
   end
 
   def test_target_empty_directory_option_l
-    contents, _message = @content_factory.create_content('./empty_dir')
-    assert_nil @ls.display({ l: true }, contents)
+    contents = ContentFactory.create_contents('./empty_dir')
+    assert_nil Ls.display({ l: true }, contents)
   end
 
   def test_show_contents_option_l
-    contents, _message = @content_factory.create_content('./alias_and_dir')
+    contents = ContentFactory.create_contents('./alias_and_dir')
     assert_equal [
       'total 8',
-      '-rw-r--r--  1  UserName  staff  936  09 08 21:42  alias_a',
-      'drwxr-xr-x  3  UserName  staff   96  09 08 21:42  child_dir',
-      '-rw-r--r--  1  UserName  staff    0  09 08 21:42  file1.txt'
-    ], @ls.display({ l: true }, contents)
+      '-rw-r--r--  1  UserName  staff  936  10 22 10:36  alias_a',
+      'drwxr-xr-x  3  UserName  staff   96  10 22 10:36  child_dir',
+      '-rw-r--r--  1  UserName  staff    0  10 22 10:36  file1.txt'
+    ], Ls.display({ l: true }, contents)
   end
 end
